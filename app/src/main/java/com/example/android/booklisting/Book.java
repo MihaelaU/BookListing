83d03832baac455b224cdfb4766d71c844644b74
MIHAELA\ursum <ursumihaela2009@gmail.com>
2017-07-08 20:29:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/booklisting/Book.java
@@ -0,0 +1,23 @@
+package com.example.android.booklisting;
+
+/**
+ * Book
+ * A custom class to store Book information
+ */
+class Book {
+
+    final String title;
+    final String authors;
+    final String smallThumbnailLink;
+
+    public Book(
+            String title,
+            String authors,
+            String smallThumbnailLink
+    ) {
+
+        this.title = title;
+        this.authors = authors;
+        this.smallThumbnailLink = smallThumbnailLink;
+    }
+}
